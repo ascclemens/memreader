e2a4d1c514869e4d9d67dcc1b9a5c0962df58060
Kyle Clemens <me@kyleclemens.com>
2017-02-24 23:59:19 -0500

    fix: set length of vector

--- src/linux.rs
+++ src/linux.rs
@@ -24,8 +24,9 @@ impl ReadsMemory for MemReader {
   fn read_bytes(&self, address: usize, n: usize) -> Result<Vec<u8>, c_int> {
     let mut file = self.get_memory_file()?;
     file.seek(SeekFrom::Start(address as u64)).map_err(|_| -2)?;
-    let mut bytes = Vec::with_capacity(n);
-    file.read(&mut bytes).map_err(|_| -3)?;
+    let mut bytes: Vec<u8> = Vec::with_capacity(n);
+    unsafe { bytes.set_len(n) };
+    file.read_exact(&mut bytes).map_err(|_| -3)?;
     Ok(bytes)
   }
 }
